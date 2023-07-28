038fb129584cbc65980dd06b85b38f6a8c0b8e6f
gkufatty <133120205+gkufatty@users.noreply.github.com>
2023-07-27 22:09:50 -0500

    Update timestamp.py

--- monitor/plotting_functions/timestamp.py
+++ monitor/plotting_functions/timestamp.py
@@ -1,21 +1,19 @@
 import matplotlib.pyplot as plt
 
 class timestamp(object):
-  
-  packets=sim_h5['packets']
-  packet_index=np.array(list(range(0,len(packets))))
-  data_packet_mask = packets['packet_type']==0
-  trig_packet_mask = packets['packet_type']==4
-  sync_packet_mask = packets['packet_type']==7
-  rollover_packet_mask=(packets['packet_type']==6) & (packets['trigger_type'] == 83)
-  other_packet_mask= ~(data_packet_mask | trig_packet_mask | sync_packet_mask | rollover_packet_mask)
-  
     def __call__(self, filename, fh, fig=None):
         if fig is not None:
             # do stuff to update plot
             pass
         else:
-            # do stuff to make new plo
+            # do stuff to make new plot
+            packets=fh['packets']
+            packet_index=np.array(list(range(0,len(packets))))
+            data_packet_mask = packets['packet_type']==0
+            trig_packet_mask = packets['packet_type']==4
+            sync_packet_mask = packets['packet_type']==7
+            rollover_packet_mask=(packets['packet_type']==6) & (packets['trigger_type'] == 83)
+            other_packet_mask= ~(data_packet_mask | trig_packet_mask | sync_packet_mask | rollover_packet_mask)
             fig = plt.figure(dpi=100)
             plt.plot(packets['timestamp'][data_packet_mask],packet_index[data_packet_mask],'o',label='data packets', linestyle='None')
             plt.plot(packets['timestamp'][trig_packet_mask],packet_index[trig_packet_mask],'o',label='trigger packets',linestyle='None')
