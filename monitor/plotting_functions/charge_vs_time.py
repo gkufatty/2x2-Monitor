40f1cd085035147b94794d0885c6f24a9458355d
gkufatty <133120205+gkufatty@users.noreply.github.com>
2023-08-01 11:18:23 -0500

    Update charge_vs_time.py

--- monitor/plotting_functions/charge_vs_time.py
+++ monitor/plotting_functions/charge_vs_time.py
@@ -1,20 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-class chargetime(object):
+class ChargePerTime(object):
     def __call__(self, filename, fh, fig=None):
         if fig is not None:
-            # do stuff to update plot
-            pass
-        else:
-            # do stuff to make new plot
-            packets=fh['packets']
-            for iogroup in range(1,9,1):
-              iogroup_mask=(packets['io_group']==iogroup) & data_packet_mask
-              plt.hist(packets['timestamp'][iogroup_mask]%SPILL_PERIOD,weights=packets['dataword'][iogroup_mask],bins=200,label='io_group '+str(iogroup),alpha=0.5)
-            plt.xlabel('timestamp%spill_period')
-            plt.ylabel('charge [ADC]')
-            plt.legend(ncol=4,bbox_to_anchor=(-0.05,1.00),loc='lower left')
-
-      
+            plt.figure(fig.number)
+            plt.close()
+            fig = None
+            
+        # do stuff to make new plot
+        fig = plt.figure(dpi=100, figsize=(10,6))
+        SPILL_PERIOD = 1.2e7
+        packets=fh['packets']
+        data_packet_mask = packets['packet_type']==0
+        for iogroup in range(1,9,1):
+            iogroup_mask=(packets['io_group']==iogroup) & data_packet_mask
+            plt.hist(packets['timestamp'][iogroup_mask]%SPILL_PERIOD,weights=packets['dataword'][iogroup_mask],bins=200,label='io_group '+str(iogroup),alpha=0.5)
+        plt.xlabel('timestamp%spill_period')
+        plt.ylabel('charge [ADC]')
+        plt.legend(ncol=4,bbox_to_anchor=(-0.05,1.00),loc='lower left')
+        
         return fig
