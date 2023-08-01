918a84ee9804201410a5847c0ae85d909fa993e8
gkufatty <133120205+gkufatty@users.noreply.github.com>
2023-08-01 16:45:22 -0500

    Update InteractionsHistogram.py

--- monitor/plotting_functions/InteractionsHistogram.py
+++ monitor/plotting_functions/InteractionsHistogram.py
@@ -12,9 +12,9 @@ class ChargePerTime(object):
         fig = plt.figure(dpi=100, figsize=(10,6))
         genie_hdr=fh['genie_hdr']
         n_vertices=np.zeros(genie_hdr['eventID'].max())
-        for i in range(len(n_vertices)):
-          n_vertices[i]=np.count_nonzero(genie_hdr['eventID']==i)
-          plt.hist(n_vertices,bins=np.arange(-0.5, n_vertices.max()+1.5,1))
+        for i in range(len(n_vertices)):    
+            n_vertices[i]=np.count_nonzero(genie_hdr['eventID']==i)
+        plt.hist(n_vertices,bins=np.arange(-0.5, n_vertices.max()+1.5,1))
         plt.title('Total interactions per spill')
         plt.xlabel('Interactions')
         plt.ylabel('Counts')
